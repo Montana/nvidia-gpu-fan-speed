ac756bbb5be42730f14ca05333b9266f4175ec6c
Montana Mendy <montana@linux.com>
2022-01-19 02:33:19 -0800

    Refactor the script myself, have this running on friends PC (I don't use PC).

--- gpufanspeed.sh
+++ gpufanspeed.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 setFanSpeed() {
-	eval "nvidia-settings -a GPUFanControlState=1 -a [fan:0]/GPUCurrentFanSpeed=$1 > /dev/null"
+    eval "nvidia-settings -a GPUFanControlState=1 -a [fan:0]/GPUCurrentFanSpeed=$1 > /dev/null"
 }
 
 cleanup() {
-	eval "nvidia-settings -a GPUFanControlState=0"
-	exit
+    eval "nvidia-settings -a GPUFanControlState=0"
+    exit
 }
 
 declare -i gpuTemp
 
 trap cleanup 1 2 3 15 20
 
-while : 
+while :
 do
+    gpuTemp=$(nvidia-settings -q gpucoretemp | grep '^  Attribute' | \
+        head -n 1 | perl -pe 's/^.*?(\d+)\.\s*$/\1/;')
+    echo -en "Current GPU temperature: $gpuTemp \r"
 
-	gpuTemp=$(nvidia-settings -q gpucoretemp | grep '^  Attribute' | \
-		head -n 1 | perl -pe 's/^.*?(\d+)\.\s*$/\1/;')
-	echo -en "Current GPU temperature: $gpuTemp \r"
+    if   [ $gpuTemp -ge 70 ]; then
+        setFanSpeed 100
+    elif [ $gpuTemp -ge 65 ]; then
+        setFanSpeed 90
+    elif [ $gpuTemp -ge 60 ]; then
+        setFanSpeed 75
+    elif [ $gpuTemp -ge 55 ]; then
+        setFanSpeed 60
+    elif [ $gpuTemp -ge 50 ]; then
+        setFanSpeed 50
+    else
+        setFanSpeed 40
+    fi
 
-	if   [ $gpuTemp -ge 70 ]; then
-		setFanSpeed 100
-	elif [ $gpuTemp -ge 65 ]; then
-		setFanSpeed 90
-	elif [ $gpuTemp -ge 60 ]; then
-		setFanSpeed 75
-	elif [ $gpuTemp -ge 55 ]; then
-		setFanSpeed 60
-	elif [ $gpuTemp -ge 50 ]; then
-		setFanSpeed 50
-	else
-		setFanSpeed 40
-	fi
-
-sleep 5
+    sleep 5
 done
